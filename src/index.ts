7f5e8bc5b0825fab16be05270de5e3dfa0faf29b
peadd2501 <98930557+peadd2501@users.noreply.github.com>
2024-11-20 11:30:59 -0600

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-export * from 'src/app/pages/id-vision/components/camara-video-selfie/camara-video-selfie.component';
-export * from 'src/app/pages/id-vision/components/camera-with-overlay/camera-with-overlay.component';
-export * from 'src/app/pages/id-vision/components/custom-slide/custom-slide.component';
-export * from 'src/app/pages/id-vision/services/dpi/dpi-service.service';
-export * from 'src/app/pages/id-vision/services/modal-services/modal-dpi-services';
-export * from 'src/app/pages/id-vision/services/modal-services/modal-video-selfie-services';
+// Exportar componentes
+export * from './app/pages/id-vision/components/camara-video-selfie/camara-video-selfie.component';
+export * from './app/pages/id-vision/components/camera-with-overlay/camera-with-overlay.component';
+export * from './app/pages/id-vision/components/custom-slide/custom-slide.component';
+// Exportar servicios
+export * from './app/pages/id-vision/services/dpi/dpi-service.service';
+export * from './app/pages/id-vision/services/modal-services/modal-dpi-services';
+export * from './app/pages/id-vision/services/modal-services/modal-video-selfie-services';
