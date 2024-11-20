a967a78074102f870a8a5ae1ff30ec2059b146cd
peadd2501 <98930557+peadd2501@users.noreply.github.com>
2024-11-20 12:02:22 -0600

    Add new exports 2

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,8 @@
 export * from './app/pages/id-vision/components/camara-video-selfie/camara-video-selfie.component';
 export * from './app/pages/id-vision/components/camera-with-overlay/camera-with-overlay.component';
 export * from './app/pages/id-vision/components/custom-slide/custom-slide.component';
+export * from './app/pages/id-vision/id-vision.component';
+export * from './app/pages/id-vision/id-vision.module';
 // Exportar servicios
 export * from './app/pages/id-vision/services/dpi/dpi-service.service';
 export * from './app/pages/id-vision/services/modal-services/modal-dpi-services';
