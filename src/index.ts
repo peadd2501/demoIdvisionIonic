353be04259c7d52cd903e91bb25a43615625127b
peadd2501 <98930557+peadd2501@users.noreply.github.com>
2024-11-20 10:27:38 -0600

    Cambios para SDK

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from 'src/app/pages/id-vision/components/camara-video-selfie/camara-video-selfie.component';
+export * from 'src/app/pages/id-vision/components/camera-with-overlay/camera-with-overlay.component';
+export * from 'src/app/pages/id-vision/components/custom-slide/custom-slide.component';
+export * from 'src/app/pages/id-vision/services/dpi/dpi-service.service';
+export * from 'src/app/pages/id-vision/services/modal-services/modal-dpi-services';
+export * from 'src/app/pages/id-vision/services/modal-services/modal-video-selfie-services';
