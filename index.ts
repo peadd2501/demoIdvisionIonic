82bd5df1fae415405b5b846d420b90b5d3c5375a
peadd2501 <98930557+peadd2501@users.noreply.github.com>
2024-11-21 11:48:07 -0600

    Fix index.js

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Exportar componentes
+export * from './src/app/pages/id-vision/components/camara-video-selfie/camara-video-selfie.component';
+export * from './src/app/pages/id-vision/components/camera-with-overlay/camera-with-overlay.component';
+export * from './src/app/pages/id-vision/components/custom-slide/custom-slide.component';
+export * from './src/app/pages/id-vision/id-vision.component';
+export * from './src/app/pages/id-vision/id-vision.module';
+// Exportar servicios
+export * from './src/app/pages/id-vision/services/dpi/dpi-service.service';
+export * from './src/app/pages/id-vision/services/modal-services/modal-dpi-services';
+export * from './src/app/pages/id-vision/services/modal-services/modal-video-selfie-services';
