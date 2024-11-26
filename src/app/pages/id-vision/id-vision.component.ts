98d5c444e4069b062139a5ed2fa38ffa2d334b46
peadd2501 <98930557+peadd2501@users.noreply.github.com>
2024-11-26 16:27:41 -0600

    Fix cambio para request cierre modal y reset camara

--- src/app/pages/id-vision/id-vision.component.ts
+++ src/app/pages/id-vision/id-vision.component.ts
@@ -18,7 +18,7 @@ register();
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [IonicModule, CommonModule, CustomSlideComponent],
-  providers: [DpiService, ModalDpiServices, ModalVideoSelfieServices],
+  // providers: [DpiService, ModalDpiServices, ModalVideoSelfieServices],
   templateUrl: './id-vision.component.html',
   styleUrls: ['./id-vision.component.scss'],
   encapsulation: ViewEncapsulation.None
